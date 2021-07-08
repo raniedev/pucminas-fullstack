1736971f704d976d771c203c97e31989ec2557ae
Ranie <raniesilvarj@gmail.com>
2021-07-08 01:34:20 -0300

    Add files via upload

--- hands-on/2-javascript/js/invocacao-imediata.js
+++ hands-on/2-javascript/js/invocacao-imediata.js
@@ -8,3 +8,52 @@ console.log(typeof ola);
 (function(nome){
     console.log(`Hello ${nome}`);
 })('Zaque');
+
+let $ = (function() {
+    let contadores = [];
+
+    function Contador(i) {
+        if(contadores[i] === undefined)
+            contadores[i] = 0;
+        this.indice = i;
+        this.incrementa = function() {
+            contadores[this.indice]++;
+            return this;
+        };
+        this.decrementa = function() {
+            contadores[this.indice]--;
+            return this;
+        };
+        this.valor = function (){
+            return contadores[this.indice];
+        }
+    }
+
+    return function(i) {
+        return new Contador(i);
+    };
+})();
+
+let c = $(3);
+console.log(c);
+
+c.incrementa();
+c.incrementa();
+c.incrementa();
+c.incrementa();
+console.log(c.valor());
+c.incrementa();
+c.incrementa();
+console.log(c.valor());
+c.decrementa();
+console.log(c.valor());
+c.incrementa();
+c.incrementa();
+c.incrementa();
+console.log(c.valor());
+
+let c2 = $(5);
+console.log(c2);
+
+c2.incrementa().incrementa().incrementa().incrementa().decrementa();
+console.log(c2.valor());
