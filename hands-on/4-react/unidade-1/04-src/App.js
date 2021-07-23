36f213abc5e1eb2dd6a587d5a53aea2ed9877223
Ranie <raniesilvarj@gmail.com>
2021-07-23 19:59:44 -0300

    Add files via upload

---
+++ hands-on/4-react/unidade-1/04-src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { TodoList } from './components/TodoList';
+
+function App() {
+  return (
+    <section>
+      <h1>Custom TODO-List</h1>
+      <TodoList />
+    </section>
+  );
+}
+
+export default App;
---
+++ hands-on/4-react/unidade-1/04-src/components/TodoListItem.js
@@ -0,0 +1,12 @@
+export const TodoListItem = (props) => {
+    return (
+        <li>
+            <input 
+                type="checkbox"
+                checked={props.item.isCompleted}
+                onChange={(event) => props.onTaskChanged(event, props.item)}
+            />
+            {props.item.name}
+        </li>
+    );
+};
