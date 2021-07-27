371e1d0e70f678d6b1f2addf255675689d811727
Ranie <raniesilvarj@gmail.com>
2021-07-27 20:25:54 -0300

    Add files via upload

---
+++ hands-on/4-react/unidade-2/02-site/src/components/Movie.js
@@ -0,0 +1,11 @@
+export const Movie = () => (
+    <div className="movie-item">
+        <div>
+            <img src="http://placehold.it/150x150" alt=""/>
+        </div>
+        <div className="movie-excerpt">
+            <h3>Batman</h3>
+            <a className="btn btn-primary" href="#">Ver detalhes</a>
+        </div>
+    </div>
+)
