07cb451319b615dfd5ddcb1cdd522913d721b133
Ranie <raniedev@gmail.com>
2021-08-06 03:49:25 -0300

    Update ApplicationRoutes.js

--- hands-on/4-react/unidade-3/02-src/ApplicationRoutes.js
+++ hands-on/4-react/unidade-3/02-src/ApplicationRoutes.js
@@ -1,11 +1,12 @@
 import { Route } from 'react-router';
 import { Home } from './views/Home';
 import { MovieDetail } from './views/MovieDetail';
+import { Favorites } from './views/Favorites';
 
 export const ApplicationRoutes = () => (
     <>
         <Route exact path="/" component={Home}></Route>
         <Route exact path="/movie/:id" component={MovieDetail}></Route>
-        <Route exact path="/favorites" component={MovieDetail}></Route>
+        <Route exact path="/favorites" component={Favorites}></Route>
     </>
 )
